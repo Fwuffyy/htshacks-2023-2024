8e1ff76247f198fcfe6c8b956e33810e468db5b4
nexus-x86 <60083031+nexus-x86@users.noreply.github.com>
2023-12-07 19:21:13 -0500

    lmao this doesnt work

--- server/index.ts
+++ server/index.ts
@@ -3,8 +3,10 @@ import { TesseractAPI } from "./tesseract";
 
 const app = express();
 
-TesseractAPI.read(); // idk
+//TesseractAPI.read(); // idk
 
 app.listen(25565, () => {
     console.log("Listening on :25565");
+    console.log("obamna");
+    console.log(TesseractAPI.read("https://tesseract.projectnaptha.com/img/eng_bw.png"))
 });
--- server/tesseract.ts
+++ server/tesseract.ts
@@ -3,6 +3,12 @@ import { createWorker } from "tesseract.js"
 export namespace TesseractAPI {
     // Code here...
 
-    export function read() {}
+    export async function read(link:string) {
+        const worker = await createWorker();
+        const ret = await worker.recognize(link);
+        return ret.data.text;
+    }
+
+
     
 }
