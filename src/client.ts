23c8952b9809ed6a30f8bd218f3576f36a4a2e60
nexus-x86 <60083031+nexus-x86@users.noreply.github.com>
2023-12-08 12:47:41 -0500

    lmao this doesnt work 5

---
+++ src/client.ts
@@ -0,0 +1,34 @@
+document.getElementById('captureButton').addEventListener('click', () => {
+    // Trigger camera capture or upload logic here
+    captureImage();
+});
+
+function captureImage() {
+    // Add code to interact with the camera or upload image
+    // For simplicity, you can use a file input to trigger image selection
+    const fileInput = document.createElement('input');
+    fileInput.type = 'file';
+    fileInput.accept = 'image/*';
+    fileInput.addEventListener('change', handleFileSelect);
+    fileInput.click();
+}
+
+function handleFileSelect(event) {
+    const file = event.target.files[0];
+    if (file) {
+        // You can use AJAX or Fetch API to send the image to the server
+        const formData = new FormData();
+        formData.append('image', file);
+
+        fetch('/upload', {
+            method: 'POST',
+            body: formData,
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Server response:', data);
+            // Handle the server response as needed
+        })
+        .catch(error => console.error('Error:', error));
+    }
+}
